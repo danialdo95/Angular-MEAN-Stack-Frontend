a2734f57685a0b4d45ef10b5ed4db454712acb3e
danialdo95 <danialdo95@gmail.com>
2018-05-21 11:39:37 +0800

    intial commit. Finish until Authentication not implement auth guard yet

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../services/validate.service';
+import { AuthService } from '../services/auth.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email:String;
+  password: String;
+
+  constructor(
+    private validateService: ValidateService, 
+    private flashMessage:FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+    
+    //Required Fields
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessage.show('Please Fill in all Fields' ,{cssClass: 'alert alert-danger', timeout: 3000});
+      return false;
+    }
+
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('You are now registered and can log in' ,{cssClass: 'alert alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      }
+      else{
+        this.flashMessage.show('Something went wrong' ,{cssClass: 'alert alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+
+  }
+
+}
